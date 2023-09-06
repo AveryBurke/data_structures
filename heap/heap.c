617ea2de8e49a9d57c7350251926d7690ed14025
Conrad <ConradWarren@1.com>
2023-09-06 08:05:20 -0600

    Added dynamic memory allocation to heap and some refactoring.

--- heap/heap.c
+++ heap/heap.c
@@ -1,52 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-enum {max, min};
+enum { max, min };
 
-typedef struct max_heap{
+#define get_parent_index(idx) ((idx-1)/2)
+#define compare(type, val_1, val_2) ((type == max) ? ((val_1 > val_2) ? 1 : 0) : ((val_1 < val_2) ? 1 : 0))
+
+typedef struct heap {
 
     int size;
     int max_size;
     int* arr;
-
     int type;
 
-}max_heap;
+}heap;
 
-void init_heap(max_heap* heap, int _max_size, int _type) {
-    heap->size = 0;
-    heap->max_size = _max_size;
-    heap->arr = (int*)calloc(_max_size, sizeof(int));
-    heap->type = _type;
+void init_heap(heap* _heap, int _type) {
+    _heap->size = 0;
+    _heap->max_size = 10;
+    _heap->arr = (int*)calloc(10, sizeof(int));
+    _heap->type = _type;
 }
 
-void destory_heap(max_heap* heap) {
-    free(heap->arr);
+void destory_heap(heap* _heap) {
+    free(_heap->arr);
 }
 
-int get_parent_index(int idx) {
-    return (idx - 1) / 2;
+int get_left_child_index(heap* _heap, int idx) {
+    return (((idx * 2) + 1) < _heap->size) ? (idx * 2) + 1 : -1;
 }
 
-int get_left_child_index(max_heap* heap, int idx) {
-    return (((idx * 2) + 1) < heap->size) ? (idx*2) + 1 : -1;
+int get_right_child_index(heap* _heap, int idx) {
+    return (((idx * 2) + 2) < _heap->size) ? (idx * 2) + 2 : -1;
 }
 
-int get_right_child_index(max_heap* heap, int idx) {
-    return (((idx * 2) + 2) < heap->size) ? (idx * 2) + 2 : -1;
+void extend_heap(heap* _heap) {
+    int* new_arr = (int*)calloc(_heap->max_size*2, sizeof(int));
+    memcpy(new_arr, _heap->arr, _heap->max_size * sizeof(int));
+    free(_heap->arr);
+    _heap->arr = new_arr;
 }
 
-int heapify_up(max_heap* heap) {
+int heapify_up(heap* _heap) {
 
-    int current_idx = heap->size - 1;
-    int parent_idx = get_parent_index(heap->size - 1);
+    int current_idx = _heap->size - 1;
+    int parent_idx = get_parent_index(_heap->size - 1);
     int temp = 0;
 
-    while ((heap->type == max && heap->arr[current_idx] > heap->arr[parent_idx]) || (heap->type == min && heap->arr[current_idx] < heap->arr[parent_idx])) {
+    while ((_heap->type == max && _heap->arr[current_idx] > _heap->arr[parent_idx]) || (_heap->type == min && _heap->arr[current_idx] < _heap->arr[parent_idx])) {
 
-        temp = heap->arr[current_idx];
-        heap->arr[current_idx] = heap->arr[parent_idx];
-        heap->arr[parent_idx] = temp;
+        temp = _heap->arr[current_idx];
+        _heap->arr[current_idx] = _heap->arr[parent_idx];
+        _heap->arr[parent_idx] = temp;
 
         current_idx = parent_idx;
         parent_idx = get_parent_index(current_idx);
@@ -55,16 +61,7 @@ int heapify_up(max_heap* heap) {
     return current_idx;
 }
 
-int compare(int type, int val_1, int val_2) {
-
-    if (type == max) {
-        return (val_1 > val_2) ? 1 : 0;
-    }
-
-    return (val_1 < val_2) ? 1 : 0;
-}
-
-void heapify_down(max_heap* heap, int current_idx) {
+void heapify_down(heap* heap, int current_idx) {
     int temp = 0;
     int left_child_idx = 0;
     int right_child_idx = 0;
@@ -80,7 +77,7 @@ void heapify_down(max_heap* heap, int current_idx) {
             heap->arr[right_child_idx] = temp;
             current_idx = right_child_idx;
         }
-        else  {
+        else {
             temp = heap->arr[current_idx];
             heap->arr[current_idx] = heap->arr[left_child_idx];
             heap->arr[left_child_idx] = temp;
@@ -92,36 +89,33 @@ void heapify_down(max_heap* heap, int current_idx) {
     }
 }
 
-void push(max_heap* heap, int val) {
+void push(heap* _heap, int val) {
 
-    if (heap->size == heap->max_size) {
-        return;
+    if (_heap->size == _heap->max_size) {
+        extend_heap(_heap);
     }
 
-    heap->arr[heap->size] = val;
+    _heap->arr[_heap->size] = val;
+    _heap->size++;
 
-    heap->size++;
-    int current_idx = heapify_up(heap);
+    int current_idx = heapify_up(_heap);
 
-    heapify_down(heap, current_idx);
+    heapify_down(_heap, current_idx);
 
 }
 
-int pop(max_heap* heap) {
+int pop(heap* _heap) {
 
-    int result = heap->arr[0];
+    int result = _heap->arr[0];
 
-    heap->size--;
-    heap->arr[0] = heap->arr[heap->size];
+    _heap->size--;
+    _heap->arr[0] = _heap->arr[_heap->size];
 
-    heapify_down(heap, 0);
+    heapify_down(_heap, 0);
 
     return result;
 }
 
-int get_top(max_heap* heap) {
-    return heap->arr[0];
+int get_top(heap* _heap) {
+    return _heap->arr[0];
 }
-//
-// Created by Conrad on 2023-09-04.
-//
